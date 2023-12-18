11dfb1ecb972fc02e9792df6d6b6eff60e137733
marcotulionasc <marconascpriv@gmail.com>
2023-12-18 14:11:55 -0300

    teste

--- api/register.php
+++ api/register.php
@@ -2,27 +2,25 @@
 
 include_once('connection.php');
 
-    // Verificar se o formulário foi enviado
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $nome = $_POST['nome'];
-        $email = $_POST['email'];
-        $senha = $_POST['senha'];
+// Verificar se o formulário foi enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
 
-        // Inserir usuário no banco de dados
-        $sql = "INSERT INTO usuarios (id, nome, email, senha) VALUES (2,'$nome', '$email', '$senha')";
+    // Inserir usuário no banco de dados
+    $sql = "INSERT INTO usuarios (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
 
-        if ($conn->query($sql) === TRUE) {
-            echo "Usuário registrado com sucesso!";
-            header("Location: login_sucess.php");
-        } else {
-            echo "Erro ao registrar o usuário: " . $conn->error;
-        }
+    if ($conn->query($sql) === TRUE) {
+        echo "Usuário registrado com sucesso!";
+        header("Location: login_sucess.php");
+    } else {
+        echo "Erro ao registrar o usuário: " . $conn->error;
     }
-
+    
     // Fechar a conexão com o banco de dados
     $conn->close();
-else {
+} else {
     die("As variáveis de ambiente do banco de dados não estão definidas. Verifique suas configurações.");
 }
-
 ?>

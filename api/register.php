741b0401764ca2835659ca795689b157d9f3a7d7
marcotulionasc <marconascpriv@gmail.com>
2023-12-18 13:41:19 -0300

    :fire: change structure

--- api/register.php
+++ api/register.php
@@ -1,28 +1,39 @@
 <?php
-include_once('connection.php');
+//session_start();
 
-// Verificar a conexão
-if ($conn->connect_error) {
-    die("Conexão falhou: " . $conn->connect_error);
-}
+// Se as variáveis de ambiente estiverem definidas, use-as para a conexão
+if (isset($_ENV["DB_HOST"], $_ENV["DB_USER"], $_ENV["DB_PASSWORD"], $_ENV["DB_NAME"], $_ENV["DB_PORT"])) {
+    $DB_HOST = $_ENV["DB_HOST"];
+    $DB_USER = $_ENV["DB_USER"];
+    $DB_PASSWORD = $_ENV["DB_PASSWORD"];
+    $DB_NAME = $_ENV["DB_NAME"];
+    $DB_PORT = $_ENV["DB_PORT"];
 
-// Verificar se o formulário foi enviado
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+    $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME, $DB_PORT);
 
-    // Inserir usuário no banco de dados
-    $sql = "INSERT INTO usuarios (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Usuário registrado com sucesso!";
-    } else {
-        echo "Erro ao registrar o usuário: " . $conn->error;
+    if ($conn->connect_error) {
+        die("Falha na conexão com o banco de dados: " . $conn->connect_error);
     }
-}
 
-// Fechar a conexão com o banco de dados
-$conn->close();
-?>
+    // Verificar se o formulário foi enviado
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+
+        // Inserir usuário no banco de dados
+        $sql = "INSERT INTO usuarios (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "Usuário registrado com sucesso!";
+        } else {
+            echo "Erro ao registrar o usuário: " . $conn->error;
+        }
+    }
+
+    // Fechar a conexão com o banco de dados
+    $conn->close();
+} else {
+    die("As variáveis de ambiente do banco de dados não estão definidas. Verifique suas configurações.");
+}
 ?>

b5e61d2f7d97411263bbba39642d3a0d809e1104
marcotulionasc <marconascpriv@gmail.com>
2023-12-18 13:38:49 -0300

    :fire: arrumando var de conn

--- api/connection.php
+++ api/connection.php
@@ -9,10 +9,10 @@ if (isset($_ENV["DB_HOST"], $_ENV["DB_USER"], $_ENV["DB_PASSWORD"], $_ENV["DB_NA
     $DB_NAME = $_ENV["DB_NAME"];
     $DB_PORT = $_ENV["DB_PORT"];
 
-    $db = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME, $DB_PORT);
+    $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME, $DB_PORT);
 
-    if ($db->connect_error) {
-        die("Falha na conexão com o banco de dados: " . $db->connect_error);
+    if ($conn->connect_error) {
+        die("Falha na conexão com o banco de dados: " . $conn->connect_error);
     }
 } else {
     die("As variáveis de ambiente do banco de dados não estão definidas. Verifique suas configurações.");
